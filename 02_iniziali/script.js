882d8bc24e84ddb672fa157ea1d706234aa15009
AlexandruMunteanu89 <sasamunteanu18@gmail.com>
2026-03-11 22:16:29 +0100

    ho fatto esercizio 2

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -1,13 +1,20 @@
-/* Scrivi una funzione che accetti un array di nomi e restituisca nuovo un array con le iniziali di ogni parola dell'array fornito */
+/* Scrivi una funzione che accetti un array di nomi e restituisca nuovo un array 
+con le iniziali di ogni parola dell'array fornito */
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 
 // Dichiara la funzione qui.
-
+function ritornaPrimaLetera(nomi){
+    const primaLetera = [];
+    for (let index = 0; index < nomi.length; index++) {
+        primaLetera.push(nomi[index].charAt(0));
+    }
+    return primaLetera;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(ritornaPrimaLetera(names));
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
