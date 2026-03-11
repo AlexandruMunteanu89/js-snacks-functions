5253201494d6fa157b3c4bc51fa183a5712a68e7
AlexandruMunteanu89 <sasamunteanu18@gmail.com>
2026-03-11 23:10:10 +0100

    risolto 04 conta vocali

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -4,10 +4,19 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
-
-
+function vocali(stringa){
+    let quantiVocali = 0;
+    const listaVocali = ['a', 'e', 'i', 'o', 'u'];
+   for (let index = 0; index < stringa.length; index++) {
+    if (listaVocali.includes(stringa.charAt(index))){
+        quantiVocali++;
+    }
+}
+return quantiVocali;
+}
 // Invoca la funzione qui e stampa il risultato in console
 
+console.log(vocali(word));
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
